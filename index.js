beec3a59e45358a9676c9faf3b40f8fee46e6843
Adeleke Hammed <lekhad19@gmail.com>
2024-03-01 17:06:57 +0100

    added resolvers functions to do the magic

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 // import { startStandaloneServer } from '@apollo/server/dist/esm/standalone';
 
+// db 
+import db from './_db.js'; 
+
 // types 
 import { typeDefs } from './schema';
 // server setup 
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        }, 
+        authors() {
+            return db.authors
+        }, 
+        reviews() {
+            return db.reviews
+        }
+    }    
+}
+
+
 const server = new ApolloServer({
     // -- definitions of types of data
-    typeDefs
-    // resolvers 
+    typeDefs, 
+    resolvers 
 
 });
 
