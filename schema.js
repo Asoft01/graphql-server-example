d234c9be2a2aabc4eab1415e32c7dbae444d9146
Adeleke Hammed Lekan <lekhad19@gmail.com>
2024-03-02 08:47:30 +0100

    add author and game by them by ID

--- schema.js
+++ schema.js
@@ -20,7 +20,9 @@ export const typeDefs = `#graphql
         reviews: [Review], 
         review(id: ID!): Review 
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
     }
 `
 // int, float, string, boolean, ID 
